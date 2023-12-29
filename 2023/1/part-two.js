c34fea278a9a41f9bb0b5d4b46e39356658944fd
Thiago Ribeiro <thiagodeev@gmail.com>
2023-12-29 19:03:20 -0300

    updated part-two. Still incompleted

--- 2023/1/part-two.js
+++ 2023/1/part-two.js
@@ -21,13 +21,54 @@ function convertNumbersToRealNumbers(string){
   return result
 }
 
+function findFirstNumber(string){
+  let numbers = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+  }
+  let arrayNumbers = Object.keys(numbers);
+
+  let firstNumber;
+
+  for (let i = 0; i < string.length;){
+    let letter = string[i];
+
+    if(Number(letter) === Number){
+      firstNumber = Number(letter);
+      break
+
+    } else {
+      let filteredNumbers = arrayNumbers.filter((numberString)=> numberString.charAt(0) == letter);
+
+      if(filteredNumbers.length > 0){
+        for (let j = 0; j < string.length;){
+          arrayNumbers.filter((numberString)=> numberString.charAt(j) == letter)
+  
+        }
+      }
+      continue
+    }
+
+    i++;
+  }
+
+  return firstNumber;
+}
+
 function findResult(input){
   let total = 0;
   
 
   input.forEach(str => {
     let adjustedStr = convertNumbersToRealNumbers(str);
-    console.log(adjustedStr)
+    // console.log(adjustedStr)
     let matches = adjustedStr.match(/\d/g);
     let result;
     
