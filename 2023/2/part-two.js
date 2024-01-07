653d934681fa32c00c7bf0a966d9f95afb299d93
Thiago Ribeiro <thiagodeev@gmail.com>
2024-01-07 13:00:43 -0300

    ended part-2. Easy!

--- 2023/2/part-two.js
+++ 2023/2/part-two.js
@@ -1,53 +1,45 @@
-// import input from "./input.js"
+import input from "./input.js"
 
-// /**
-//  * @param {Array<Object.<string, number>>} game
-//  * @returns {bool}
-// */
-// function isPossible(game){
-//   let maxValues = {
-//     red: 12,
-//     green: 13,
-//     blue: 14
-//   }
+/**
+ * @param {Array<Object.<string, number>>} game
+ * @returns {bool}
+*/
+function getPower(game){
+  let red = 0;
+  let green = 0;
+  let blue = 0;
 
-//   for(let i = 0; i < game.length; i++){
-//     if(game[i].red != undefined) {
-//       if(game[i].red > maxValues.red){
-//         return false
-//       }
-//     } 
-//     if(game[i].green != undefined) {
-//       if(game[i].green > maxValues.green){
-//         return false
-//       }
-//     } 
-//     if(game[i].blue != undefined) {
-//       if(game[i].blue > maxValues.blue){
-//         return false
-//       }
-//     } 
-//   }
-//   return true
-// }
+  for(let i = 0; i < game.length; i++){
+    if(game[i].red > red){
+      red = game[i].red;
+    }
+     
+    if(game[i].green > green){
+      green = game[i].green;
+    }
+     
+    if(game[i].blue > blue){
+      blue = game[i].blue;
+    }
+     
+  }
+  return red * green * blue
+}
 
-// /**
-//  * @param {Array<Object.<string, number>>[]} input
-//  * @returns {number}
-// */
-// function findResult(input){
-//   let total = 0;
+/**
+ * @param {Array<Object.<string, number>>[]} input
+ * @returns {number}
+*/
+function findResult(input){
+  let total = 0;
 
-//   for(let i = 0; i < input.length; i++){
-//     if(isPossible(input[i])){
-//       total += (i + 1)
-//       console.log('Game ' + (i + 1) + ' success. Total: ' + total)
-//     }
-//   }
+  for(let i = 0; i < input.length; i++){
+    total += getPower(input[i])
+  }
   
-//   return total
-// }
+  return total
+}
 
-// let total = findResult(input)
-// // console.log(input[9])
-// console.log("Part-two: The sum of the games IDs is: " + total)
+let total = findResult(input)
+// console.log(input[9])
+console.log("Part-two: The sum of the power is: " + total)
